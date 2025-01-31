34ae20183b0fec6d259d9a034ecbff52e139a063
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-01-31 14:36:33 -0500

    autofill room code (redirect if we don't have user's name)

--- src/components/JoinRoom.js
+++ src/components/JoinRoom.js
@@ -1,21 +1,34 @@
 import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 export default function JoinRoom({socket}) {
     const router = useRouter();
     const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
+    const [roomCode, setRoomCode] = useState('');
+
+    useEffect(()=>{
+        if(router.query.room){
+            setRoomCode(router.query.room);
+        }
+
+    },[router.query.room]);
 
     const handleSubmit = (name, room, option) => {
-        if (!name) {
+        if (!name || name.trim()==='') {
             alert("Please enter your name");
             return;
         }
-        socket.emit('join-room', {room, name});
+        if (option === 'join' && (!room || room.trim()==='')) {
+            alert("Please enter a room code");
+            return;
+        }
+        const sendRoomCode = option === 'create'
+            ? Math.random().toString(36).substring(2, 8)
+            : room;
 
-        const path = option === 'create' ? `/${room}` : `/${room}`;
-        router.push(path);
+        socket.emit('join-room', {room:sendRoomCode, name});
+        router.push(`/${sendRoomCode}`);
     };
 
   return (
@@ -29,15 +42,16 @@ export default function JoinRoom({socket}) {
 
           {/*create new room*/}
           <br></br>
-          <button type="submit" onClick={(e)=>handleSubmit(name,Math.random().toString(36).substring(2, 8), 'create')}>Create Room</button>
+          <button type="submit" onClick={(e)=>handleSubmit(name, null, 'create')}>Create Room</button>
+
           <p>or</p>
 
           {/*join existing room*/}
           <label htmlFor="roomName">Enter room code</label>
-        <input id="roomName" type="text" onChange={(e) => setRoom(e.target.value)}/>
+        <input id="roomName" type="text" value={roomCode} onChange={(e) => setRoomCode(e.target.value)} disabled={router.query.room !== undefined}/>
           <br></br>
 
-          <button type="submit" onClick={(e)=>handleSubmit(name, room, 'join')}>Join Room</button>
+          <button type="submit" onClick={(e)=>handleSubmit(name, roomCode, 'join')}>Join Room</button>
       </form>
     </div>
   );
